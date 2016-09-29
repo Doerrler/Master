686aa8f28c0ef6740163cee4df639e88427d88ae
Kyle <kyledoerrler@gmail.com>
2016-09-29 12:19:33 +0800

    Added Assaignment 2 Files

---
+++ indiv_test/app/unit_tests/todo_test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+//
+// TODO: Rewrite todo_test.js to achieve 100% statement/branch coverage
+//
+
+describe('TodoApp module', function() {
+   
+
+   describe('Dummy Unit Test', function() {
+	  	  
+		  it('Dummy Test function 1', function() {
+			expect("Dummy").toEqual("Dummy");
+		  });
+		  
+		  it('Dummy Test function 2', function() {
+			expect("Dummy2").toEqual("Dummy2");
+		  });
+		  
+		  it('Dummy Test function 3', function() {
+			expect("Dummy3").toEqual("Dummy3");
+		  });
+
+   });
+
+
+
+});
